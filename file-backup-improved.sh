83e2dd15e494653f32e9b658eceec9ee043ae4ab
Nivedit Soni <92633706+Nivedit84@users.noreply.github.com>
2025-07-21 15:55:40 +0530

    Update file-backup-improved.sh

--- file-backup-improved.sh
+++ file-backup-improved.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-OUTPUT_DIR="/home/ec2-user"
-INPUT_DIR=$1
+OUTPUT_DIR="$2"
+INPUT_DIR="$1"
 TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S")
 BACKUP_DIR="${INPUT_DIR}_$TIMESTAMP.tar.gz"
 
@@ -12,6 +12,6 @@ echo "BACKUP of $INPUT_DIR has been created and stored at $OUTPUT_DIR"
 
 if [ -e $BACKUP_DIR ]
 then
-        file_backup=$(find "$OUTPUT_DIR" -type f -name "$BACKUP_DIR" -mtime 7)
+        file_backup=$(find "$OUTPUT_DIR" -type f -mtime 7)
         rm $file_backup
 fi
