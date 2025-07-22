17d1f77fb657419bbbb704300313afb05193a11a
Nivedit84 <niveditsoni84@gmail.com>
2025-07-22 23:25:44 +0530

    Revised version of bulk-user-creation

--- bulk-user-creation.sh
+++ bulk-user-creation.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-output_file=user_password.txt
- 
+
+OUTPUT_FILE="$HOME/user-pass.txt"
+touch "$OUTPUT_FILE"
+
 for name in $(cat names.txt)
 do
-#-m: creates the home directory automatically
-sudo useradd -m "$name"
-#Generating a random password
-password=$(< /dev/urandom tr -dc 'A-Za-z0-9' | head -c10)
-#chpasswd takes input in the form of username:password, assigns the password to user
-echo "$name:$password" | sudo chpasswd
-#chage -d 0 (sets the password change date to 0) , asks user to change the password at next login
-sudo chage -d 0 "$name"
-echo "$name:$password" >> $output_file
+    sudo useradd -m "$name"
+    password=$(< /dev/urandom tr -dc 'A-Za-z0-9' | head -c10)
 
+    echo "$name:$password" | sudo chpasswd
+    sudo chage -d 0 "$name"
+
+    echo "$name:$password" >> "$OUTPUT_FILE"
 done
