eb25d8a6406ad02cabd8090bfbf04282077bf09f
Nivedit84 <niveditsoni84@gmail.com>
2025-07-22 14:30:13 +0530

    IPTABLES config for allowing SSH, HTTP and HTTPS traffic

---
+++ firewall-automation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum install iptables
+
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -A INPUT -j DROP
+
+sudo service iptables save
