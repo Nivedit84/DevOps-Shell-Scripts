5e88c4ccb2ef242248503a789a5237541a0d0ec7
Nivedit Soni <92633706+Nivedit84@users.noreply.github.com>
2025-07-22 16:37:52 +0530

    Update firewall-automation.sh
    Updated script using conntrack module

--- firewall-automation.sh
+++ firewall-automation.sh
@@ -2,6 +2,8 @@
 
 sudo yum install iptables
 
+sudo iptabels -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
