6c212252e156e1beaaf622d42fe7224a91db56aa
Nivedit Soni <92633706+Nivedit84@users.noreply.github.com>
2025-07-28 15:30:11 +0530

    Update log-cleaner.sh
    Corrected logical error

--- log-cleaner.sh
+++ log-cleaner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(find /var/log -type f -name "*.log" -mtime +10) 
+for file in $(find /var/log -type f -name "*.log" -mtime 10) 
 do   
     rm -f $file 
     echo "This file has been deleted: $file"
