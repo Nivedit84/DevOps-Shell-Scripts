97787b1e803d83f29381b7e18f4dc232ee401b58
Nivedit Soni <92633706+Nivedit84@users.noreply.github.com>
2025-07-30 20:56:10 +0530

    Update nginx-log-analyser.sh
    Corrected logical error

--- nginx-log-analyser.sh
+++ nginx-log-analyser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_FILE="$1"
+file="$1"
 
 if [ ! -f "$1" ]
 then
@@ -8,16 +8,14 @@ then
         exit 1
 fi
 
-IP_ADDRESSES=$(cat $LOG_FILE | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 5)
+ips=$(cat $file | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 5)
 echo "Top 5 IP addresses"
-echo $IP_ADDRESSES
+echo $ips
 echo
-REQUESTED_PATHS=$(cat $LOG_FILE | sed -n 's/.*\(GET\|\POST|\PUT|\DELETE\) \([^ ]*\) .*/\2/p' | sort | uniq -c | sort -nr | head -n 5)
-echo "Top 5 paths"
-echo $REQUESTED_PATHS
+paths=$(cat $file | sed -n 's/.*\(GET\|POST\|PUT\|DELETE\) \([^ ]*\) .*/\2/p' | sort | uniq -c | sort -r | head -n 5)
+echo "Top 5 requested paths"
+echo $paths
 echo
-STATUS_CODES=$(cat $LOG_FILE | awk '{print $9}' | sort | uniq -c | sort -nr | head -n 5)
-echo "Top status codes"
-echo $STATUS_CODES
-echo
-#display $IP_ADDRESSES $REQUESTED_PATHS $STATUS_CODES
+status=$(cat $file | sed -n 's/.*\" \([0-9]\{3\}\).*/\1/p' | sort | uniq -c | sort -r | head -n 5)
+echo "Top 5 HTTP codes"
+echo $status
