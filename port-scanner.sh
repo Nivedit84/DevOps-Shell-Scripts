b764e23ba82ff7664f2c3eb20f1ba66c6ac6ebf6
Nivedit Soni <92633706+Nivedit84@users.noreply.github.com>
2025-08-02 18:30:51 +0530

    Update port-scanner.sh

--- port-scanner.sh
+++ port-scanner.sh
@@ -2,7 +2,7 @@
 
 for port in $(cat port.txt); do
     if sudo ss -tuln | awk '{print $5}' | grep -q ":$port\$"; then
-        echo "Port: $port is open"
+        echo "Port: $port is bound to a process and expecting traffic"
     else
         echo "Port $port is closed"
     fi
