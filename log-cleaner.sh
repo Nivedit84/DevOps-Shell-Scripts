e1bae046671fa92172706a2fe38e593851ad0e21
Nivedit Soni <92633706+Nivedit84@users.noreply.github.com>
2025-11-19 18:49:57 +0530

    Fix find command to delete logs older than 10 days

--- log-cleaner.sh
+++ log-cleaner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for file in $(find /var/log -type f -name "*.log" -mtime 10) 
+for file in $(find /var/log -type f -name "*.log" -mtime +10);
 do   
     rm -f $file 
     echo "This file has been deleted: $file"
 done
+
