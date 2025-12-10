2fefc72af7885e7516754176754fc1320d90c4f8
Nivedit Soni <92633706+Nivedit84@users.noreply.github.com>
2025-12-10 15:38:42 +0530

    Enhance bulk-user-creation.sh with comments
    Added comments to clarify useradd and chage commands.

--- bulk-user-creation.sh
+++ bulk-user-creation.sh
@@ -5,11 +5,11 @@ touch "$OUTPUT_FILE"
 
 for name in $(cat $INPUT_FILE)
 do
-    sudo useradd -m "$name"
+    sudo useradd -m "$name"  #-m creates a home directory for the user
     password=$(< /dev/urandom tr -dc 'A-Za-z0-9' | head -c10)
 
     echo "$name:$password" | sudo chpasswd
-    sudo chage -d 0 "$name"
+    sudo chage -d 0 "$name" #chage -d is change age, prompts user to enter a new password at the next login
 
     echo "$name:$password" >> "$OUTPUT_FILE"
 done
